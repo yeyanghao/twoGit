a0dad709e7b1924425cbea57738f5e7bbda29922
qinting <qinting@Qinting-MacBook.local>
2016-08-23 18:43:32 +0800

    viewDidload

--- 喵优先/喵优先/ViewController.m
+++ 喵优先/喵优先/ViewController.m
@@ -16,7 +16,16 @@
 @implementation ViewController
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.scorllView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
+    self.scorllView.contentSize = CGSizeMake(self.view.bounds.size.width, self.view.bounds.size.height*2);
+    HeaderViewController *headerVC = [[HeaderViewController alloc]init];
+    headerVC.view.frame = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height*0.4);
+    [self addChildViewController:headerVC];
+    [self.scorllView addSubview:headerVC.view];
+    [self.view addSubview:self.scorllView];
+    self.scorllView.showsVerticalScrollIndicator = NO;
+    self.scorllView.bounces = NO;
+    self.scorllView.alwaysBounceHorizontal = YES;
 }
 -(void)createSV{
     self.scorllView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
